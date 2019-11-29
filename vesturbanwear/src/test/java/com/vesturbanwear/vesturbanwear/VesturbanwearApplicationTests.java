8083ab54c2c294d9bd38cf89533679710a7b3e4a
Ila <Ila@LAPTOP-T8MVC3AM>
2019-11-28 23:28:49 -0300

    Commit inicial

---
+++ vesturbanwear/src/test/java/com/vesturbanwear/vesturbanwear/VesturbanwearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vesturbanwear.vesturbanwear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VesturbanwearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
